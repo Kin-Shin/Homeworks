4df50191e02a12118742285570f7b897f87f8d2e
Shin <akinshinivan87@gmail.com>
2021-12-04 18:04:50 +0300

    delete homework 2 from branch 3

--- task2_2.py
+++
@@ -1,5 +0,0 @@
-random_list = input("Введите элементы списка: ")
-split_list = random_list.split()
-split_list[:-1:2], split_list[1::2] = split_list[1::2], split_list[:-1:2]
-print(split_list)
-
--- task4_2.py
+++
@@ -1,3 +0,0 @@
-var_words = input("Введите набор слов ").split()
-for num, elem in enumerate(var_words, 1):
-    print(num, elem[:10])
